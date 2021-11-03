e195819b857d02ab483e0f34ec70e93e15aaea5c
Alfred Ang <angch@tertiaryinfotech.com>
2021-11-04 02:21:57 +0800

    1103

--- sample codes/Topic2/2-1/app.py
+++ sample codes/Topic2/2-1/app.py
@@ -1,23 +1,15 @@
-import time
-
-import redis
 from flask import Flask
+import os
+import socket
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-@app.route('/')
+@app.route("/")
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    html = "<h3>Hello {name}!</h3>" \
+    		"This page is created inside the container"
+
+    return html.format(name=os.getenv("NAME", "world"))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
